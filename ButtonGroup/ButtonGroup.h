18c6b0c7c53b3e6aca8f20ce677660a2229349d4
Jesper <gedemagt@gmail.com>
2014-04-03 16:46:35 +0200

    moved the files to a folder

---
+++ ButtonGroup/ButtonGroup.h
@@ -0,0 +1,32 @@
+
+#ifndef BUTTONGROUP_H
+#define BUTTONGROUP_H
+
+#include "Arduino.h"
+
+struct button {
+	int buttonState;
+	int lastButtonState;
+	int pin;
+	int mode;
+	void (*f)();
+	long last_rising;
+};
+
+class ButtonGroup {
+public:
+	ButtonGroup(int dimension);
+	~ButtonGroup();
+	// mode = 0: falling edge
+	// mode = 1: rising edge
+	// mode = 2: long click
+	void addButton(int pin, void (*f)(), int mode);
+	void loopButtons();
+	void setLongClickThreshold(long threshold);
+
+private:
+	button* buttons;
+	int count;
+	long long_click_threshold;
+};
+#endif
